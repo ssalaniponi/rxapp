cd249cdd5d773ab14e72f6768c427df00aa2b619
ssalaniponi <steven_salaniponi@yahoo.co.uk>
2016-01-14 22:25:08 +0200

    Account model and related files

---
+++ app/models/account.rb
@@ -0,0 +1,19 @@
+class Account < ActiveRecord::Base
+	validates_presence_of :shopify_account_url
+	validates_presence_of :shopify_api_key
+	validates_presence_of :shopify_password
+	validates_presence_of :shopify_shared_secret
+
+	# This should return the minimal set of attributes required
+	# to create a valid Account. As you add validations to
+	# Account, be sure to adjust the attributes here as well.
+
+	describe "valid attributes" do
+		let(:valid_attributes) { { 
+			"shopify_account_url" => "MyString", 
+			"shopify_password" => "MyString",
+			"shopify_api_key" => "MyString", 
+			"shopify_shared_secret" => "MyString" 
+		} }
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rxapp::Application.routes.draw do
+  resources :accounts
+
   get "dashboard/index"
   root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.

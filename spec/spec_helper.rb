4018abcb76b50001ea462a2689b506c3d5c22889
ssalaniponi <steven_salaniponi@yahoo.co.uk>
2016-01-15 07:04:19 +0200

    Test_19

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'rspec/rails'
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 
